44d04565ffc4122511330ff29309f742cf0f7ea5
luispiura89 <luispiura@unitec.edu>
2020-08-12 17:25:43 -0600

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Luis Francisco Piura Mejia on 8/12/20.
+//  Copyright © 2020 Luis Francisco Piura Mejia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
