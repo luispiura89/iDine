ebdf04a249dffc501664cd06bfcab17c5c8d6f7b
luispiura89 <luispiura@unitec.edu>
2020-08-13 12:18:23 -0600

    Add item details view

--- iDine/ItemRow.swift
+++ iDine/ItemRow.swift
@@ -20,30 +20,32 @@ struct ItemRow: View {
     var item: MenuItem
     
     var body: some View {
-        HStack {
-            Image(item.thumbnailImage)
-                .clipShape(Circle())
-                .overlay(
-                    Circle()
-                        .stroke(Color.gray, lineWidth: 2)
-            )
-            VStack(alignment: .leading) {
-                Text(item.name)
-                    .font(.headline)
-                Text("$\(item.price)")
-            }
-            .layoutPriority(1)
-            
-            Spacer()
-            
-            ForEach(item.restrictions, id: \.self) { restriction in
-                Text(restriction)
-                    .font(.caption)
-                    .fontWeight(.black)
-                    .padding(5)
-                    .background(Self.colors[restriction])
+        NavigationLink(destination: ItemDetail(item: item)) {
+            HStack {
+                Image(item.thumbnailImage)
                     .clipShape(Circle())
-                    .foregroundColor(.white)
+                    .overlay(
+                        Circle()
+                            .stroke(Color.gray, lineWidth: 2)
+                )
+                VStack(alignment: .leading) {
+                    Text(item.name)
+                        .font(.headline)
+                    Text("$\(item.price)")
+                }
+                .layoutPriority(1)
+                
+                Spacer()
+                
+                ForEach(item.restrictions, id: \.self) { restriction in
+                    Text(restriction)
+                        .font(.caption)
+                        .fontWeight(.black)
+                        .padding(5)
+                        .background(Self.colors[restriction])
+                        .clipShape(Circle())
+                        .foregroundColor(.white)
+                }
             }
         }
     }
