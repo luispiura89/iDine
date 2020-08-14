e01e9640aa4b85f36fcb8c642048f57bc4caaa59
luispiura89 <luispiura@unitec.edu>
2020-08-14 12:34:12 -0600

    Add ChecoutView

---
+++ iDine/CheckoutView.swift
@@ -0,0 +1,66 @@
+//
+//  CheckoutView.swift
+//  iDine
+//
+//  Created by Luis Francisco Piura Mejia on 8/14/20.
+//  Copyright © 2020 Luis Francisco Piura Mejia. All rights reserved.
+//
+
+import SwiftUI
+
+struct CheckoutView: View {
+    @EnvironmentObject var order: Order
+    
+    static let paymentTypes = ["Cash", "Credit Card", "iDine Points"]
+    static let tipAmounts = [10, 15, 20, 25, 0]
+    
+    @State private var paymentType = 0
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
+    @State private var tipAmount = 1
+    
+    var body: some View {
+        Form {
+            Section {
+                Picker("How do you want to pay?", selection: $paymentType) {
+                    ForEach(0 ..< Self.paymentTypes.count) {
+                        Text(Self.paymentTypes[$0])
+                    }
+                }
+                
+                Toggle(isOn: $addLoyaltyDetails.animation()) {
+                    Text("Add iDine loyalty card")
+                }
+                
+                if addLoyaltyDetails {
+                    TextField("Enter your iDine ID", text: self.$loyaltyNumber)
+                }
+            }
+            
+            Section(header: Text("Add a tip?")) {
+                Picker("Percentage", selection: $tipAmount) {
+                    ForEach(0 ..< Self.tipAmounts.count) {
+                        Text("\(Self.tipAmounts[$0])%")
+                    }
+                }
+                .pickerStyle(SegmentedPickerStyle())
+            }
+            
+            Section(header: Text("TOTAL: $100")) {
+                Button("Confirm order") {
+                    
+                }
+            }
+        }
+        .navigationBarTitle(Text("Payment"), displayMode: .inline)
+    }
+}
+
+struct CheckoutView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            CheckoutView()
+                .environmentObject(Order())
+        }
+    }
+}
